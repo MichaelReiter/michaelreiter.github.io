80169a90dee2068ce44f0103e49a79e7e32f51c8
Michael Reiter <michaelreiter@live.ca>
2015-09-06 11:10:08 -0700

    Update about page transition

--- js/about.js
+++ js/about.js
@@ -3,5 +3,5 @@ var fadeInDuration = 2000;
 
 $(document).ready(function() {
   $('.fadeIn').removeClass('notShown').fadeIn(fadeInDuration/5);
-  $('.slide').addClass('slideUp');
+  $('.slide').removeClass('notShown').addClass('slideUp');
 });
