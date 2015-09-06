9842ee31fa1a4c9c72ee89677d78219a35890b96
Michael Reiter <michaelreiter@live.ca>
2015-09-06 10:14:16 -0700

    Rewrite about page markup and styles

--- js/scripts.js
+++ js/scripts.js
@@ -2,9 +2,6 @@ var delay = 1000;
 var fadeInDuration = 2000;
 
 $(document).ready(function() {
-	$('.fadeIn').removeClass('notShown').fadeIn(fadeInDuration/5);
-	$('.slide').addClass('slideUp');
-	
   $('.first').fadeIn(fadeInDuration).removeClass('hidden');
   $('.second').delay(delay).fadeIn(fadeInDuration).removeClass('hidden');
   $('.third').delay(delay*2).fadeIn(fadeInDuration).removeClass('hidden');
