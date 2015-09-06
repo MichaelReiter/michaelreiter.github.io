89787f60fa3c983f674a7fefd6181b03e179bff6
Michael Reiter <michaelreiter@live.ca>
2015-09-06 11:30:29 -0700

    Update landing page transitions

--- js/scripts.js
+++ js/scripts.js
@@ -2,8 +2,8 @@ var delay = 1000;
 var fadeInDuration = 2000;
 
 $(document).ready(function() {
-  $('.first').fadeIn(fadeInDuration).removeClass('hidden');
-  $('.second').delay(delay).fadeIn(fadeInDuration).removeClass('hidden');
-  $('.third').delay(delay*2).fadeIn(fadeInDuration).removeClass('hidden');
-  $('.fourth').delay(delay*3).fadeIn(fadeInDuration).removeClass('hidden');
+  $('.first').fadeIn(fadeInDuration);
+  $('.second').delay(delay).fadeIn(fadeInDuration);
+  $('.third').delay(delay*2).fadeIn(fadeInDuration);
+  $('.fourth').delay(delay*3).fadeIn(fadeInDuration);
 });
