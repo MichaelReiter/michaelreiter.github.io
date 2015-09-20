c6c8e333253a3abf82a8b5fdcb22ca150d76e945
Michael Reiter <michaelreiter@live.ca>
2015-09-20 11:21:26 -0700

    Add Do You Even List to the projects page

---
+++ js/doyouevenlist.js
@@ -0,0 +1,3 @@
+if ($(window).width() < 720) {
+  window.location.href = "https://play.google.com/store/apps/details?id=com.michaelreiter.dyel";
+}
--- js/projects.js
+++ js/projects.js
@@ -6,6 +6,7 @@ $(document).ready(function() {
     document.getElementById("ki").setAttribute('href', "https://itunes.apple.com/app/ki-music-writing-helper-for/id955086812");
     document.getElementById("spaceblaze").setAttribute('href', "https://itunes.apple.com/app/space-blaze-simple-yet-addicting/id968067298");
     document.getElementById("randomcolorpicker").setAttribute('href', "https://itunes.apple.com/app/random-color-picker-easy-tool/id979101026");
+    document.getElementById("doyouevenlist").setAttribute('href', "https://play.google.com/store/apps/details?id=com.michaelreiter.dyel");
   }
 
 	setTimeout(function() {
